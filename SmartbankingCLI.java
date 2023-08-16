7751d36c8cc15f323c07c94cfcda9ae81cb95bbc
Sudam Shaluka <rkshaluka@gmail.com>
2023-08-16 08:30:16 +0530

    upto Assignmnet

--- SmartbankingCLI.java
+++ SmartbankingCLI.java
@@ -23,6 +23,7 @@ public class SmartbankingCLI{
 
         String screen = DASHBOARD;
         String[] customerNames = new String[0] ;
+        String[] cutomerIDarray = new String[0] ;
 
         do {
             final String APP_TITLE = String.format("%s%s%s",COLOR_BLUE_BOLD, screen, RESET);
@@ -41,6 +42,7 @@ public class SmartbankingCLI{
                     System.out.println("7] Exit");
                     System.out.print("\n"+"Enter option :");
                     int option = scanner.nextInt() ;
+                    scanner.nextLine();
                     switch(option){
                         case 1 : screen = CREATE_ACCOUNT ; break ;
                         case 2 : screen = DEPOSITS ; break ;
@@ -53,49 +55,90 @@ public class SmartbankingCLI{
                     }
                     break;
                 case CREATE_ACCOUNT :
-                    System.out.printf("\tNew Student ID: S%03d \n", (customerNames.length + 1));
+                    System.out.printf("\tNew Student ID: S-%03d\n", (customerNames.length + 1));
 
                     boolean valid;
                     String name;
                     do{
-                        valid = true;
+                        valid = false;
                         System.out.print("\tEnter Student Name: ");
                         name = scanner.nextLine().strip();
                         if (name.isBlank()){
-                            System.out.printf("\t%sName can't be empty%s\n", COLOR_RED_BOLD, RESET);
-                            valid = false;
+                            System.out.printf(ERROR_MSG,"Name cannot be empty!!");
+                            valid = true;
                             continue;
                         }
                         for (int i = 0; i < name.length(); i++) {
                             if (!(Character.isLetter(name.charAt(i)) || 
                                 Character.isSpaceChar(name.charAt(i))) ) {
                                 System.out.printf("\t%sInvalid Name%s\n", COLOR_RED_BOLD, RESET);
-                                valid = false;
+                                valid = true;
                                 break;
                             }
                         }
-                    }while(!valid);
+                    }while(valid);
                     String[] newCustomerNames = new String[customerNames.length+1] ;
+                    String[] newCustomerIDArray = new String[customerNames.length+1] ;
                     for(int i = 0 ; i < customerNames.length ; i++){
                         newCustomerNames[i] = customerNames[i] ;
+                        newCustomerIDArray[i] = cutomerIDarray[i] ;
                     }
                     newCustomerNames[newCustomerNames.length - 1] = name ;
+                    newCustomerIDArray[newCustomerIDArray.length - 1] = String.format("SDB-%05d", cutomerIDarray.length) ;
                     customerNames = newCustomerNames ;
+                    cutomerIDarray = newCustomerIDArray ;
                     int initialDeposit ;
                     do {
+                        valid = false ;
                         System.out.print("\tEnter Initial Deposit :");
                         initialDeposit = scanner.nextInt() ;
+                        scanner.nextLine() ;
                         if(initialDeposit<=5000){
-                            System.out.println("\tInitial deposit should higher that 5000");
-                            continue ;
-                        }else valid = false ;
+                            System.out.printf(ERROR_MSG,"Iniial ammount should be higher than 5000");
+                            valid = true ;
+                            continue;
+                        }
                         
-                    } while (!valid);
-                    System.out.printf("ID SDB:%05d , Name: %s Added successfully!",(customerNames.length+1),name);
-                    System.out.print("Do you want to add another{Y/N} :");
+                    } while (valid);
+                    System.out.printf("\t%sID SDB:%05d | Name: %s Added successfully!%s\n",COLOR_GREEN_BOLD,customerNames.length,name,RESET);
+                    System.out.print("\tDo you want to add another{Y/N} :");
                     if (scanner.nextLine().strip().toUpperCase().equals("Y")) continue;
                     screen = DASHBOARD;
                     break;
+                case DEPOSITS :
+                    String accNumber ;
+                    loop :
+                    do {
+                        valid = false ;
+                        System.out.print("Enter Account Number :");
+                        accNumber = scanner.nextLine().strip() ;
+                        if(accNumber.isBlank()){
+                            System.out.printf(ERROR_MSG,"Acc number cannot be empty");
+                            valid = true ;
+                            continue ;
+                        }
+                        if(accNumber.startsWith("SDB-") || accNumber.length()<5 ){
+                            System.out.printf(ERROR_MSG, "Invalid Format!") ;
+                        }
+                        for(int i = 4 ; i < accNumber.length(); i++){
+                            if( !Character.isDigit(accNumber.charAt(i)) ){
+                                 System.out.printf(ERROR_MSG,"Invalid Format");
+                                 continue loop ;
+                            }
+                        }
+                        int index = -1;
+                        for(int i = 0 ; i < cutomerIDarray.length ; i++){
+                          if(accNumber.equalsIgnoreCase(cutomerIDarray[i])){
+                            index = i ;
+                            break ;
+                          }
+                        }
+                        if(index<0){
+                            System.out.printf(ERROR_MSG,"Account Number not found!!");
+                            valid = true ;
+                            continue ;
+                        }
+                    } while (valid);
             }
         } while (true);
 
